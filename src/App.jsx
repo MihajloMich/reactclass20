6e6aef3bd274a5812d380825a4900bfca64a715e
MihajloMich <mihajloodimov03@gmail.com>
2023-01-18 20:02:30 +0100

    adding prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import Student from "./Components/Student"
-import StudentClass from "./Components/StudentClass"
-
-
-
-
-  return (<div>
-    
-  </div>);
-
-
-
+const App = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
 export default App;
